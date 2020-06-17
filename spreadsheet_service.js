10c03411b3d328f6998ae5e093fa1de0e20152f3
rhc822 <rhc822@gmail.com>
2020-06-17 08:17:51 -0500

    Completed second lesson of 3rd module: Apps Script + Google Sheets

--- spreadsheet_service.js
+++ spreadsheet_service.js
@@ -17,7 +17,7 @@ function getData(){
  const sheet = ss.getActiveSheet(); // Sheet the cursor is in
  const data = sheet.getDataRange();
  const values = data.getValues();
-
+  
  Logger.log(values)
 
 }
@@ -50,7 +50,7 @@ function getData3(){
 
   const ss = SpreadsheetApp.getActiveSpreadsheet();
   const sheet1 = ss.getSheetByName("Sheet1");
-
+  
   Logger.log(sheet1.getDataRange().getValues());
 
 }
@@ -62,11 +62,11 @@ function getData4(){
 
   const ss = SpreadsheetApp.getActiveSpreadsheet();
   const sheet1 = ss.getSheetByName("Sheet1");
-
+  
   const active = sheet1.getActiveRange().getValues();
-
+  
   Logger.log(active);
-
+  
   // [[Fruit, Cost], [Apple, 0.86]]
 
 }
@@ -77,11 +77,11 @@ function getData5(){
 
   const ss = SpreadsheetApp.getActiveSpreadsheet();
   const sheet1 = ss.getSheetByName("Sheet1");
-
+  
   const data = sheet1.getRange("A1:C4").getValues();
 
   Logger.log(data);
-
+  
   // [[Fruit, Cost, Quantity], [Apple, 0.86, 4.0], [Banana, 0.82, 2.0], [Pear, 0.52, 7.0]]
 
 }
@@ -94,25 +94,55 @@ function getData6(){
   const sheet1 = ss.getSheetByName("Sheet1");
 
   const data = sheet1.getRange(1, 1, 4, 1).getValues();
-
+  
   // [[Fruit], [Apple], [Banana], [Pear]]
-
+  
   const data2 = sheet1.getRange(3, 1, 4, 3).getValues();
-
+  
   Logger.log(data2);
-
+  
   // [[Banana, 0.82, 2.0], [Pear, 0.52, 7.0], [Watermelon, 1.74, 2.0], [Kiwi, 0.99, 3.0]]
 
-
+  
 }
 
 
 
+/*************************************************************************************/
 
 
+// calculate total for each row
 
+function calculateRowTotal() {
 
+  var ss = SpreadsheetApp.getActive();
+  var sheet1 = ss.getSheetByName("Sheet1");
+  
+  var data = sheet1.getRange(2, 1, 6, 3).getValues();
+  
+//   Logger.log(data);
+  
+  data.forEach(function(row) {
+    
+    // Logger.log(row); // [Apple, 0.86, 4.0]
+    
+    // Logger.log(row[0]);
+    // Logger.log(row[1]);
+    // Logger.log(row[2]);
+    
+    var name = row[0];
+    var cost = row[1];
+    var quantity = row[2];
+    
+    var total = cost * quantity;
+    
+    // Logger.log(total);
+    Logger.log(name + ": $" + total);
+    
+  });
 
+}
+  
 
 
 
